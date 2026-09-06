2bf7a3ff88e2271233f3a87797ada3bd750aced2
İsmail Caner <126804265+ismailcaner@users.noreply.github.com>
2026-09-06 18:15:37 +0300

    Remove unused submit environment setting

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['v5.airtableusercontent.com'], // Resimlerin yükleneceği alan adı
+    domains: ['v5.airtableusercontent.com'],
   },
   env: {
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
     REACT_APP_API_BASE_ID: process.env.REACT_APP_API_BASE_ID,
     REACT_APP_DATA_TABLE_NAME: process.env.REACT_APP_DATA_TABLE_NAME,
-    REACT_APP_USER_TABLE_NAME: process.env.REACT_APP_USER_TABLE_NAME
   },
 };
 
